e0710011b7c7f51e2bba643853954b94e3495ea8
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-06-06 11:03:30 +0530

    Create Sort012_number.cpp

---
+++ Sort012_number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int temp = 0, low = 0, mid = 0, high = nums.size()-1;
+        
+        while(mid <= high) {
+            
+            if(nums[mid] == 0) {
+                temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid]==1) {
+                mid++;
+            } else if(nums[mid]==2) {
+                temp  = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+    }
+};
